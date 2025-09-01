9837033c3d46faaa0582b075e7535bb9d0d4cd13
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2025-09-01 23:09:39 +0700

    make api company

---
+++ app/Http/Controllers/Api/CompanyController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Company;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\CompanyResource;
+
+class CompanyController extends Controller
+{
+    public function index()
+    {
+        // Fetch all companies
+        $companies = Company::get();
+        return CompanyResource::collection($companies);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string|max:1000',
+        ]);
+
+        $company = Company::create($validated);
+        return new CompanyResource($company);
+    }
+
+    public function update(Request $request, Company $company)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string|max:1000',
+        ]);
+
+        $company->update($validated);
+        return new CompanyResource($company);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CompanyController;
 use App\Http\Controllers\Api\CategoryController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
@@ -12,6 +13,10 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/categories', [CategoryController::class, 'index']);
     Route::post('/category', [CategoryController::class, 'store']);
     Route::put('/category/{category}', [CategoryController::class, 'update']);
+
+    Route::get('/companies', [CompanyController::class, 'index']);
+    Route::post('/company', [CompanyController::class, 'store']);
+    Route::put('/company/{company}', [CompanyController::class, 'update']);
 });
 
 require __DIR__ . '/auth.php';
